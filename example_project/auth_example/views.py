6e00a8868786cb3f41580a9a2da54f083e7867d7
Michael van Tellingen <michaelvantellingen@gmail.com>
2011-11-26 12:58:22 +0100

    Simplify the form handling in the example app

--- example_project/auth_example/views.py
+++ example_project/auth_example/views.py
@@ -3,27 +3,24 @@ from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy as _
 from auth_remember.auth import remember_user
+
 from example_project.auth_example.forms import LoginForm
 
 
 def login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        data = form.cleaned_data
 
-            user = auth.authenticate(
-                username=data['username'], password=data['password'])
+        user = auth.authenticate(
+            username=data['username'], password=data['password'])
 
-            if user:
-                auth.login(request, user)
-                if data['remember_me']:
-                    remember_user(request, user)
-            else:
-                form.add_non_field_error(_("Invalid username/password"))
-
-    else:
-        form = LoginForm()
+        if user:
+            auth.login(request, user)
+            if data['remember_me']:
+                remember_user(request, user)
+        else:
+            form.add_non_field_error(_("Invalid username/password"))
 
     return TemplateResponse(request, 'login.html', {
         'form': form,
