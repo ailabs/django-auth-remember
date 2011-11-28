e82131ba53c06e4dc8835658324d532f4aa857d1
Michael van Tellingen <michaelvantellingen@gmail.com>
2011-11-28 14:43:19 +0100

    Prefix global settings with AUTH_REMEMBER_ instead of REMEMBER_ME_

--- auth_remember/settings.py
+++ auth_remember/settings.py
@@ -5,5 +5,5 @@ COOKIE_AGE = 86400 * 28  # 4 weeks by default
 SESSION_KEY = 'AUTH_REMEMBER_FRESH'
 
 for k in dir(settings):
-    if k.startswith('REMEMBER_ME_'):
-        locals()[k.split('REMEMBER_ME_', 1)[1]] = getattr(settings, k)
+    if k.startswith('AUTH_REMEMBER_'):
+        locals()[k.split('AUTH_REMEMBER_', 1)[1]] = getattr(settings, k)
