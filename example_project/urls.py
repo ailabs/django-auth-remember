9e61353eb802da28a9a7832874cdf5a1e9107349
Michael van Tellingen <michaelvantellingen@gmail.com>
2011-11-28 10:42:06 +0100

    Activate the admin in the example app

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'auth_example.views.login', name='auth_login'),
     url(r'^logout$', 'auth_example.views.logout', name='auth_logout'),
+
+    # Django Admin
+    url(r'^admin/', include(admin.site.urls)),
 )
